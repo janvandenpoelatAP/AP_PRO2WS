07f1725514af5560737c191fbe242bf8acaae8d9
janvandenpoelatAP <jan.vandenpoel@ap.be>
2022-10-17 07:31:03 +0200

    Les 04 ASP.NET CORE

---
+++ Les 04 ASP.NET CORE/HelloCore/HelloCore/Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloCore.Controllers;
+
+public class HomeController
+{
+    public string Index()
+    {
+        return "Hello from the HomeController";
+    }
+}
---
+++ Les 04 ASP.NET CORE/HelloCore/HelloCore/Program.cs
@@ -0,0 +1,9 @@
+using HelloCore;
+
+await Host.CreateDefaultBuilder(args)
+    .ConfigureWebHostDefaults(webBuilder =>
+    {
+        webBuilder.UseStartup<Startup>();
+    })
+    .Build()
+    .RunAsync();
